0f2dad4b1a066cad207d99d779e9080421cf43af
paulgreenwood67 <109767559+paulgreenwood67@users.noreply.github.com>
2023-07-20 14:02:14 +0100

    Update graphics.js

--- pages/graphics.js
+++ pages/graphics.js
@@ -35,6 +35,10 @@ const Projects = () => {
                      
 
                <h3>Social assets for the Freshwater Habitat Trust</h3>
+         <video controls width="640" height="360">
+        <source src={fht1} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
                 <Image className='graphic' src ={fht1} />
                 <Image className='graphic' src ={fht2} />    
                <h3>Park signage for Lancashire Wildlife Trust</h3>
